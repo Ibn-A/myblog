8913162586717b204ed967f76a0151e9e524b42b
Ibn <ibn_ali@yahoo.com>
2020-09-14 13:33:04 +0200

    Mise en place d'une pagination simple - buttons pages suivantes et pages précedentes.

--- views/post/index.php
+++ views/post/index.php
@@ -6,7 +6,22 @@ $title = "Mon Blog";
 $pdo = new PDO('mysql:host=127.0.0.1;dbname=blog', 'root', 'root', [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
-$query = $pdo->query('SELECT * FROM post ORDER BY created_at DESC LIMIT 12');
+// numéro de la page courante
+$currentPage = (int)($_GET['page'] ?? 1);
+if ($currentPage <= 0) {
+    throw new Exception('Numéro de page invalide');
+}
+//récupère le nombre total d'articles
+$count = (int)$pdo->query('SELECT COUNT(id_post) FROM post')->fetch(PDO::FETCH_NUM)[0];
+$perPage = 12;
+//récupère le nombre total de pages
+$pages = ceil($count / $perPage);
+if ($currentPage > $pages) {
+    throw new Exception('Cette page n\'existe pas');
+}
+//récupère les 12 dernières articles triés par date
+$offset = $perPage * ($currentPage - 1);
+$query = $pdo->query("SELECT * FROM post ORDER BY created_at DESC LIMIT $perPage OFFSET $offset");
 $posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
 
 ?>
@@ -18,5 +33,16 @@ $posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
         <?php require 'card.php' ?>
     </div>
     <?php endforeach ?>
+</div>
 
+<div class="d-flex justify-content-between my-4">
+        <?php if ($currentPage > 1):?>
+            <?php 
+            $link = $router->url('home');
+            ?>
+            <a href="<?= $link ?>?page=<?= $currentPage - 1 ?>" class="btn btn-primary">&laquo; Page précédente</a>
+        <?php endif?>
+        <?php if ($currentPage < $pages):?>
+            <a href="<?= $link ?>?page=<?= $currentPage + 1 ?>" class="btn btn-primary ml-auto"> Page suivante &raquo;</a>
+        <?php endif?>
 </div>
