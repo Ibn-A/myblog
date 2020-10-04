f515e4e2c4af3a0c0184e0a8cd8c11e2bb8c9475
Ibn <ibn_ali@yahoo.com>
2020-10-04 16:21:51 +0200

    Validation du formulaire d'edit d'un post :  - Gestion de la validation du Formulaire & Affichage des messages d'erreurs.

--- views/admin/post/edit.php
+++ views/admin/post/edit.php
@@ -8,10 +8,21 @@ $postTable = new PostTable($pdo);
 $post = $postTable->find($params['id']);
 $success = false;
 
+$errors = [];
+// conditions de validation du formulaire
 if (!empty($_POST)) {
-    $post->setTitle($_POST['name']);
-    $postTable->update($post);
-    $success = true;
+    if (empty($_POST['title'])) {
+        $errors['title'][] = 'Le champs titre ne peut pas être vide';
+    }
+    if (mb_strlen($_POST['title'])  <= 3) {
+        $errors['title'][] = 'Le champs titre doit contenir plus de 3 caractères';
+    }
+    $post->setTitle($_POST['title']);
+    if (empty($errors)) {
+        $postTable->update($post);
+        $success = true;
+    }
+    
 }
 ?>
 
@@ -20,13 +31,23 @@ if (!empty($_POST)) {
         L'article #<?=$post->getID()?> a bien été modifié.
     </div>
 <?php endif ?>
+<?php if (!empty($errors)):?>
+    <div class="alert alert-danger">
+        L'article n'a pu être modifié, merci de corriger vos erreurs.
+    </div>
+<?php endif ?>
 
 <h1> Editer l'article : <?= he($post->getTitle())?></h1>
 
 <form action="" method="POST">
     <div class="form-group">
         <label for="name" >Titre</label>
-        <input type="text" class="form-control" name="name" value="<?= he($post->getTitle())?>" required>
+        <input type="text" class="form-control <?= isset($errors['title']) ? 'is-invalid' : '' ?>" name="title" value="<?= he($post->getTitle())?>">
+        <?php if (isset($errors['title'])):?>
+        <div class="invalid-feedback">
+            <?= implode('<br>', $errors['title'])?>
+        </div>
+        <?php endif ?>
     </div>
     <button class="btn btn-primary">Modifier</button>
 </form>
