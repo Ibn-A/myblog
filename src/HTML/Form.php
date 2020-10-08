129bfc34116b1d91aec3c5619b0f158588d51686
Ibn <ibn_ali@yahoo.com>
2020-10-08 12:07:07 +0200

    Modification fichier Form.php : -Modification_method getvalue()

--- src/HTML/Form.php
+++ src/HTML/Form.php
@@ -41,7 +41,7 @@ HTML;
 
     }
 
-    private function getValue(string $key): string {
+    private function getValue(string $key): ?string {
         if (is_array($this->data)) {
             return $this->data[$key] ?? null;
         }
