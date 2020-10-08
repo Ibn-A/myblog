19544bde23c212a68875e1ce13637a7f21ada355
Ibn <ibn_ali@yahoo.com>
2020-10-08 08:02:36 +0200

    Refactoring_Validation Formulaire : - Mise en place de la class parente AbstractValidator.php

--- src/Validators/PostValidator.php
+++ src/Validators/PostValidator.php
@@ -1,35 +1,22 @@
 <?php
 namespace App\Validators;
 
-use App\Validator;
 use App\Table\PostTable;
 
-class PostValidator {
-
-    private $data;
-    private $validator;
+class PostValidator extends AbstractValidator {
 
     public function __construct(array $data, PostTable $table, ?int $postID = null) {
-        $this->data = $data;
-        $v = new Validator($data);
-        $v->rule('required', ['title','slug']);
-        $v->rule('lengthBetween', ['title','slug'], 5, 100);
-        $v->rule('slug', 'slug');
+        parent::__construct($data);
+        $this->validator->rule('required', ['title','slug']);
+        $this->validator->rule('lengthBetween', ['title','slug'], 5, 100);
+        $this->validator->rule('slug', 'slug');
         //règle pour éviter les doublons de slug dans la bdd___A revoir
         /*
-        $v->rule(function($field, $value) use ($table, $postID) {
-            $field ='slug_post';
+        $this->validator->rule(function($field, $value) use ($table, $postID) {
+            $field = 'slug_post';
             return !$table->exists($field, $value, $postID);
         },['slug'], 'Cette valeur est déjà utilisée');
         */
-        $this->validator = $v;
     }
 
-    public function validate(): bool {
-        return $this->validator->validate();
-    }
-
-    public function errors(): array {
-        return $this->validator->errors();
-    }
 }
