533042018a4e85ab2edbf21e88a73539b37c0025
Ibn <ibn_ali@yahoo.com>
2020-10-12 10:26:29 +0200

    Mise en place de la gestion des catégories: - Ajout des différentes routes.

--- public/index.php
+++ public/index.php
@@ -31,9 +31,17 @@ $router = new App\Router(dirname(__DIR__) . '/views');
 $router->get('/', 'post/index', 'home');
 $router->get('/blog/category/[*:slug]-[i:id]', 'category/show','category');
 $router->get('/blog/[*:slug]-[i:id]', 'post/show', 'post');
+//PARTIE ADMINISTRATION
+//Gestion des articles
 $router->get('/admin', 'admin/post/index', 'admin_posts');
 $router->match('/admin/post/[i:id]', 'admin/post/edit', 'admin_post');
 $router->post('/admin/post/[i:id]/delete', 'admin/post/delete', 'admin_post_delete');
 $router->match('/admin/post/new', 'admin/post/new', 'admin_post_new');
+//Gestion des catégories
+$router
+->get('/admin/categories', 'admin/category/index', 'admin_categories')
+->match('/admin/category/[i:id]', 'admin/post/edit', 'admin_category')
+->post('/admin/category/[i:id]/delete', 'admin/post/delete', 'admin_category_delete')
+->match('/admin/category/new', 'admin/category/new', 'admin_category_new');
 $router->run();
 
