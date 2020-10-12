88e89bc22bbdc47b2afe7b3be596d6d8b2150a15
Ibn <ibn_ali@yahoo.com>
2020-10-12 10:27:37 +0200

    Mise en place de la gestion des catégories : - Ajout des setteurs sur model Category.php

--- src/Model/Category.php
+++ src/Model/Category.php
@@ -16,12 +16,27 @@ class Category {
         return $this->id_category;
     }
 
+    public function setID(int $id): self {
+        $this->id_category = $id;
+        return $this;
+    }
+
     public function getName(): ?string {
         return $this->name_category;
     }
 
+    public function setName(string $name): self {
+        $this->name = $name;
+        return $this;
+    }
+
     public function getSlug(): ?string {
         return $this->slug_category;
+    }
+
+    public function setSlug($slug): self {
+        $this->slug_category = $slug;
+        return $this;
     }
 
     public function getPostID(): ?int {
