3248d8f6fe9ceff45b215dcd4eca027ad0f78980
Ibn <ibn_ali@yahoo.com>
2020-10-13 18:42:50 +0200

    Modification des routes pour la gestion_categories

--- public/index.php
+++ public/index.php
@@ -40,8 +40,8 @@ $router->match('/admin/post/new', 'admin/post/new', 'admin_post_new');
 //Gestion des catégories
 $router
 ->get('/admin/categories', 'admin/category/index', 'admin_categories')
-->match('/admin/category/[i:id]', 'admin/post/edit', 'admin_category')
-->post('/admin/category/[i:id]/delete', 'admin/post/delete', 'admin_category_delete')
+->match('/admin/category/[i:id]', 'admin/category/edit', 'admin_category')
+->post('/admin/category/[i:id]/delete', 'admin/category/delete', 'admin_category_delete')
 ->match('/admin/category/new', 'admin/category/new', 'admin_category_new');
 $router->run();
 
