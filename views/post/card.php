025f6be60ffc1f5a485ef399684262a779cc4ce9
Ibn <ibn_ali@yahoo.com>
2020-10-20 11:50:25 +0200

    Update card.php

--- views/post/card.php
+++ views/post/card.php
@@ -7,21 +7,25 @@ foreach($post->getCategories() as $category) {
 HTML;
 }
 ?>
+<div class="col-lg-4 mb-4">
+    <div class="entry2">
+        <div class="card mb-3">
+            <div class="card-body">
+                <h5 class="card-title"><?= htmlentities($post->getTitle()) ?></h5>
+                <p class="text-muted">
+                    <?= $post->getCreatedAt()->format('d F Y')?> 
+                    <?php if (!empty($post->getCategories())) :?>
+                    ::
+                    <?= implode(', ', $categories) ?>
+                    <?php endif ?>
+                </p>
+                <p><?= $post->getExcerpt() ?></p>
+                <p>
+                    <a href="<?= $router->url('post', ['id' => $post->getID(), 'slug' => $post->getSlug()])?>" class="btn btn-primary">voir plus</a>
+                </p>
 
-<div class="card mb-3">
-    <div class="card-body">
-        <h5 class="card-title"><?= htmlentities($post->getTitle()) ?></h5>
-        <p class="text-muted">
-            <?= $post->getCreatedAt()->format('d F Y')?> 
-            <?php if (!empty($post->getCategories())) :?>
-            ::
-            <?= implode(', ', $categories) ?>
-            <?php endif ?>
-        </p>
-        <p><?= $post->getExcerpt() ?></p>
-        <p>
-            <a href="<?= $router->url('post', ['id' => $post->getID(), 'slug' => $post->getSlug()])?>" class="btn btn-primary">voir plus</a>
-        </p>
+            </div>
+        </div>
 
     </div>
 </div>
