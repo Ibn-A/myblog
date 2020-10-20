210fc3c5588b2f136dba11b8193f9574e8a28800
Ibn <ibn_ali@yahoo.com>
2020-10-20 15:01:08 +0200

    Update rule on CategoryValidator.php

--- src/Validators/CategoryValidator.php
+++ src/Validators/CategoryValidator.php
@@ -8,7 +8,7 @@ class CategoryValidator extends AbstractValidator {
     public function __construct(array $data, CategoryTable $table, ?int $id = null) {
         parent::__construct($data);
         $this->validator->rule('required', ['name','slug']);
-        $this->validator->rule('lengthBetween', ['name','slug'], 5, 100);
+        $this->validator->rule('lengthBetween', ['name','slug'], 3, 100);
         $this->validator->rule('slug', 'slug');
         //règle pour éviter les doublons de slug dans la bdd___A revoir
         /*
